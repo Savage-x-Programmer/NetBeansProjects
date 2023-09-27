f3fd60b1d2c37746c6f503f74aefdfab38a72113
Savage-x-Programmer <143861419+Savage-x-Programmer@users.noreply.github.com>
2023-09-27 00:41:53 -0500

    MP4DataStructures

---
+++ MP4DataStructures/src/main/java/edu/slcc/asdv/mp4datastructures/MP4DataStructures.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.slcc.asdv.mp4datastructures;
+
+/**
+ *
+ * @author angel
+ */
+public class MP4DataStructures {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
