5983b9eb149ffd35df314ce8b71a6bcaaba004f7
Savage-x-Programmer <143861419+Savage-x-Programmer@users.noreply.github.com>
2023-10-16 10:07:46 -0500

    new MP5

---
+++ mp5datStruc/src/main/java/angel/mp5datstruc/ContactFixedSizeReadWriteUpdate.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package angel.mp5datstruc;
+
+/**
+ *
+ * @author angelfigeralperezestevez
+ */
+public class ContactFixedSizeReadWriteUpdate {
+    
+}
---
+++ mp5datStruc/src/main/java/angel/mp5datstruc/Mp5datStruc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package angel.mp5datstruc;
+
+/**
+ *
+ * @author angelfigeralperezestevez
+ */
+public class Mp5datStruc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
